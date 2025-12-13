7e3d5f76f79398034eb4775d8f13f2e387a350f9
marcelorodriguezmonroy-boop <marcelorodriguezmonroy@gmail.com>
2025-12-13 12:24:12 -0500

    tkinter

--- dia1/6-tkinter.py
+++ dia1/6-tkinter.py
@@ -1,18 +1,29 @@
-from tkinter import * #importamos la librería tkinter, * significa importar todo
+from tkinter import *
 
-#creamos la ventana principal
+def saludar():
+    nombre = txt_nombre.get()
+    print(f"Hola, {nombre}!")
+    
+#creamos un objeto de la clase Tk
 app = Tk()
-#titulo de la ventana
-app.title("Mi primera app con Tkinter")
-#tamaño de la ventana
-app.geometry("400x300")
+#título de la ventana
+app.title("Mi primera aplicación con Tkinter")
+#dimensiones de la ventana
+app.geometry("300x100")
 
 #crear un objeto frame
-frame = Frame(app) #el frame es un contenedor dentro de la ventana principal
-frame.grid(row=0,column=0) #ubicamos el frame en la ventana con grid
+frame = Frame(app)
+frame.grid(row=0,column=0,padx=20,pady=20)
 
-
-
+#crear una etiqueta (label) dentro del frame
+lb_nombre = Label(frame,text="Nombre :")
+lb_nombre.grid(row=0,column=0)
+#crear una caja de texto (entry) dentro del frame
+txt_nombre = Entry(frame)
+txt_nombre.grid(row=0,column=1)
+#crear un botón (button) dentro del frame
+btn_saludar = Button(frame,text="Saludar",command=saludar)
+btn_saludar.grid(row=1,column=0)
 
 #mostramos la ventana
-app.mainloop() #mantiene la ventana abierta y en espera de eventos
+app.mainloop()
