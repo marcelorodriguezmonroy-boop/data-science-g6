12aa48cbb39564813f4b398163679ebd8198271f
marcelorodriguezmonroy-boop <marcelorodriguezmonroy@gmail.com>
2025-12-23 18:52:59 -0300

    ejemplo basico

--- dia3/python-mysql/1-ejemplobasico.py
+++ dia3/python-mysql/1-ejemplobasico.py
@@ -10,3 +10,18 @@ connection = mysql.connector.connect(
 
 print(f'estas conectado a la base de datos {connection.database}')
 
+# alumno_cursor = connection.cursor()
+# alumno_cursor.execute("insert into alumno (nro_documento, nombre) values ('1002', 'Jesus Lopez')")
+# connection.commit()
+# print("Alumno insertado correctamente")
+# connection.close()
+
+alumno_cursor_select = connection.cursor()
+alumno_cursor_select.execute("select nro_documento, nombre from alumno")
+resultado = alumno_cursor_select.fetchall()
+for registro in resultado:
+    print ('*'*20)
+    print(f'Nro Documento: {registro[0]}')
+    print (f'Nombre: {registro[1]}')
+
+connection.close()
