f0ec42cfbca4e793311e53e53a6088416543b84a
marcelorodriguezmonroy-boop <marcelorodriguezmonroy@gmail.com>
2026-01-04 11:12:19 -0300

    dia4

--- dia4/api/1-requests.py
+++ dia4/api/1-requests.py
@@ -1,1 +1,20 @@
 
+import requests
+
+
+    
+
+URL = 'https://randomuser.me/api/?nat=es&results=100'
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    print('conexión a api exitosa')
+    data = response.json()
+    rows = []
+    for dic_user in data['results']:
+        nombre = dic_user['name']['first'] + ' ' + dic_user['name']['last']
+        pais = dic_user['location']['country']
+        email = dic_user['email']
+        telefono = dic_user['phone']
+        foto = dic_user['picture']['large']
