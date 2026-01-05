adb73249d3c925b1b1288c9a9a52b4d66f27c8ba
marcelorodriguezmonroy-boop <marcelorodriguezmonroy@gmail.com>
2026-01-05 20:35:19 -0300

    dia4

--- dia4/api/1-requests.py
+++ dia4/api/1-requests.py
@@ -1,10 +1,9 @@
-
 import requests
-
+from tabulate import tabulate
 
     
 
-URL = 'https://randomuser.me/api/?nat=es&results=100'
+URL = 'https://randomuser.me/api/?nat=es&results=5' 
 
 response = requests.get(URL)
 
@@ -18,3 +17,10 @@ if response.status_code == 200:
         email = dic_user['email']
         telefono = dic_user['phone']
         foto = dic_user['picture']['large']
+        rows.append([nombre, pais, email, telefono, foto])
+    headers = ['Nombre', 'País', 'Email', 'Teléfono', 'Foto']
+    print(tabulate(rows,headers=headers, tablefmt='grid'))
+
+
+else:
+    print(f'{response.status_code} error en la conexión a la api')
